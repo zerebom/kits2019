b9aadf202bba8bd6484fcfa83c91b46f4f8b93d2
higuchi <higuchi@higuchi-ubuntu18.04>
2019-07-11 10:45:17 +0900

    adapt for linux and add information whether include cancer or not

--- scripts/Preprocess/max_slice.py
+++ scripts/Preprocess/max_slice.py
@@ -10,13 +10,19 @@ from PIL import Image, ImageOps
 import matplotlib.pyplot as plt  # 描画用
 import scipy.interpolate as interpolate
 import pandas as pd
-
+from pathlib import Path
+# from Utils.status import ON_WIN
+import argparse
 
 def rotate(image, angle, center):
     h, w = image.shape[:2]
     affine = cv2.getRotationMatrix2D(center, angle, 1.0)
     return cv2.warpAffine(image, affine, (w, h))
 
+def load_npy(cid,vol_path,seg_path):
+    raw_vol=np.load(vol_path)
+    raw_seg=np.load(seg_path)
+    return raw_vol,raw_seg
 
 def load_data(cid, seg_path, vol_path):
     raw_seg = nib.load(seg_path)
@@ -28,27 +34,35 @@ def load_data(cid, seg_path, vol_path):
     return raw_vol, raw_seg
 
 
-def get_slice_idx(raw_seg):
-    rnumber = len(raw_seg[0, 0, :])
+def get_slice_idx(raw_seg,axis=0):
+    if axis==0:rnumber = len(raw_seg[:, 0, 0])
+    if axis==1:rnumber = len(raw_seg[0, :, 0])
+    if axis==2:rnumber = len(raw_seg[0, 0, :])    
+    
+    
     imagefragarray = []
     sliceIndex = []
 
     # 高さ方向の腎臓、腎臓がんの範囲特定
     for x in range(rnumber):
 
-        imagefragarray = raw_seg[:, :, x]
+        if axis==0:imagefragarray = raw_seg[x, :, :]
+        if axis==1:imagefragarray = raw_seg[:, x, :]
+        if axis==2:imagefragarray = raw_seg[:, :, x]
 
         if np.where(imagefragarray != 0, True, False).any():
             sliceIndex.append(x)
 
     return sliceIndex
 
-def get_max_datas(seg_voxel,sliceIndex):
+def get_max_datas(seg_voxel,sliceIndex,axis):
     max_area = 0
     max_rect = 0
     max_idx = 0
     for idx in sliceIndex:
-        slice_seg = seg_voxel[:, :, idx]
+
+        slice_seg =get_slice(seg_voxel,idx,axis)
+
         contours, hierarchy = cv2.findContours(slice_seg, 1, 2)
 
         max_id = np.argmax(np.array([cv2.contourArea(cnt) for cnt in contours]))
@@ -91,7 +105,7 @@ def get_cut_area(max_rect,PADDING_SIZE=20):
     PAD_LX = LX - PADDING_SIZE if(LX - PADDING_SIZE) > 0 else 0
     PAD_RX = RX + PADDING_SIZE
 
-    return(PAD_LX,PAD_RX,PAD_TY,PAD_UY)
+    return [PAD_TY, PAD_UY, PAD_LX, PAD_RX]
 
 
 def divide_index(sliceIndex):
@@ -104,16 +118,17 @@ def divide_index(sliceIndex):
             max_diff = diff
             slice1 = sliceIndex[:x]
             slice2 = sliceIndex[x:]
-            divide_Idx = sliceIndex[x - 1]
+            divide_idx = sliceIndex[x - 1]
     
     return slice1,slice2
 
 
 
-
-def rotate_cut (slice_im,TY,UY,LX,RX,degree,center,IM_SIZE,sitk_flg=True):
+#P...padding座標
+def rotate_cut (slice_im,P,degree,center,IM_SIZE,sitk_flg=True):
     rotate_a = rotate(slice_im, degree, center)
-    clip_im = rotate_a[TY:UY, LX:RX]
+    clip_im = rotate_a[P[0]:P[1], P[2]:P[3]]
+
     clip_im = cv2.resize(clip_im, dsize=(IM_SIZE, IM_SIZE), interpolation=cv2.INTER_LINEAR)
     
     if sitk_flg:
@@ -121,22 +136,52 @@ def rotate_cut (slice_im,TY,UY,LX,RX,degree,center,IM_SIZE,sitk_flg=True):
     return clip_im
 
 
-def make_paths(IMAGE_PATH,LABEL_PATH,cid,i,idx,prefix):
-    os.makedirs(os.path.join(IMAGE_PATH, 'case_00' + str(cid).zfill(3)),exist_ok=True)
-    os.makedirs(os.path.join(LABEL_PATH, 'case_00' + str(cid).zfill(3)),exist_ok=True)
+def make_paths(IMAGE_PATH,cid,i,idx,prefix,suffix):
+    OUTPUT_PATH=IMAGE_PATH/f'case_00{str(cid).zfill(3)}'
+    os.makedirs(str(OUTPUT_PATH),exist_ok=True)
+    os.makedirs(str(OUTPUT_PATH/'image'),exist_ok=True)
+    os.makedirs(str(OUTPUT_PATH/'label'),exist_ok=True)
+
     
-    label_path = os.path.join(LABEL_PATH, 'case_00' + str(cid).zfill(3), f"{prefix}_{str(i).zfill(3)}_{idx}_label.mha")
-    image_path = os.path.join(IMAGE_PATH, 'case_00' + str(cid).zfill(3), f"{prefix}_{str(i).zfill(3)}_{idx}_image.mha")
+    image_path =str(OUTPUT_PATH/'image'/ f"{prefix}_{str(i).zfill(3)}_{idx}_{suffix}_image.mha")
+    label_path = str(OUTPUT_PATH/'label'/ f"{prefix}_{str(i).zfill(3)}_{idx}_{suffix}_label.mha")
 
-    return label_path,image_path
+    return image_path,label_path
 
 
+def get_slice(raw_np,idx,axis=0):
+    if axis==0:slice_np = raw_np[idx, :, :]
+    if axis==1:slice_np = raw_np[:, idx, :]
+    if axis==2:slice_np = raw_np[:, :, idx]
+    return slice_np
 
-if __name__ == "__main__":
-    ROOT_PATH = r"C:\Users\higuchi\Desktop\kits19\data\case_00"
-    IMAGE_PATH = r"C:\Users\higuchi\Desktop\LAB\201906_\segmentation\data\image"
-    LABEL_PATH = r"C:\Users\higuchi\Desktop\LAB\201906_\segmentation\data\label"
-    errorcidtxt = r"C:\Users\higuchi\Desktop\LAB\201906_\segmentation\data\error_cid.txt"
+def cancer_flg(seg_slice):
+    if np.where(seg_slice != 2, True, False).any():
+        return 'has_2'
+    else:
+        return 'no_2'
+
+def rotate_slice(raw_vol,raw_seg,idx,axis,PADS,degree,center,IM_SIZE):
+    slice_vol = get_slice(raw_vol,idx,axis=axis)
+    slice_seg = get_slice(raw_seg,idx,axis=axis)
+    vol = rotate_cut(slice_vol, PADS, degree, center, IM_SIZE)
+    seg = rotate_cut(slice_seg, PADS, degree, center, IM_SIZE)
+    return vol,seg
+
+
+def main(parser):
+    parser.axis=0
+    if parser.axis==0:AXIS='sagittal'
+    if parser.axis==1:AXIS='coronal'
+    if parser.axis==2:AXIS='axial'
+
+    # KITS_PATH = Path('~/Desktop/kits19/data/')
+    DATA_PATH = Path('data/input/raw')
+    IMAGE_PATH=DATA_PATH.parent/'max'/AXIS
+    
+
+    os.makedirs(IMAGE_PATH,exist_ok=True)
+    error_cid_txt=str(IMAGE_PATH/'error_cid.txt')
 
     PADDING_SIZE = 20
     IM_SIZE=256 
@@ -160,36 +205,37 @@ if __name__ == "__main__":
             'maxcid_2' ])
 
     for cid in tqdm(range(210)):
-        seg_path = os.path.join(ROOT_PATH + str(cid).zfill(3), "segmentation.nii.gz")
-        vol_path = os.path.join(ROOT_PATH + str(cid).zfill(3), "imaging.nii.gz")
+        vol_path = DATA_PATH / f'case_00{str(cid).zfill(3)}'/ "image.npy"
+        seg_path = DATA_PATH / f'case_00{str(cid).zfill(3)}'/ "label.npy"
 
-        raw_vol, raw_seg = load_data(cid, seg_path, vol_path)
+        vol_path=vol_path.resolve()
+        seg_path=seg_path.resolve()
+
+
+        raw_vol, raw_seg = load_npy(cid,str(vol_path),str(seg_path))
+        raw_seg=raw_seg.astype(np.uint8)
+
         sliceIndex = get_slice_idx(raw_seg)
-        
         slice1, slice2 =divide_index(sliceIndex)
-        
-            
+        # print(len(slice1),len(slice2))
+                    
         try:
-            max_area1, max_rect1, max_idx1=get_max_datas(raw_seg, slice1)
-            max_area2, max_rect2, max_idx2=get_max_datas(raw_seg, slice2)
+            max_area1, max_rect1, max_idx1=get_max_datas(raw_seg, slice1,parser.axis)
+            max_area2, max_rect2, max_idx2=get_max_datas(raw_seg, slice2,parser.axis)
         except:
             print('max_rect is zero at :', cid)
-            with open(errorcidtxt, mode='w') as f:
-                f.write(str(cid))
+            with open(error_cid_txt, mode='w') as f:f.write(str(cid))
             continue
 
-            
         if (max_rect1 == 0) or (max_rect2 == 0):
             print('max_rect is zero at :',cid)
-            with open(errorcidtxt, mode='w') as f:
-                f.write(str(cid))
+            with open(error_cid_txt, mode='w') as f:f.write(str(cid))
             continue
         
         center1, size1, degree1 = max_rect1
         center2, size2, degree2 = max_rect2
-
-        PAD_LX1, PAD_RX1, PAD_TY1, PAD_UY1 = get_cut_area(max_rect1)
-        PAD_LX2, PAD_RX2, PAD_TY2, PAD_UY2 = get_cut_area(max_rect2)
+        PADS1= get_cut_area(max_rect1)
+        PADS2= get_cut_area(max_rect2)
 
         datalist = [seg_path, PADDING_SIZE, max_area1, size1[0], size1[1], center1[0], center1[1], degree1, max_idx1,
                     max_area2, size2[0], size2[1], center2[0], center2[1], degree2, max_idx2]
@@ -198,29 +244,37 @@ if __name__ == "__main__":
         log_df = log_df.append(log_row, ignore_index=True)
 
         for i, idx in enumerate(slice1):
-            slice_seg = raw_seg[:, :, idx]
-            slice_vol = raw_vol[:, :, idx]
-            
-            seg = rotate_cut(slice_seg, PAD_TY1, PAD_UY1, PAD_LX1, PAD_RX1, degree1, center1, IM_SIZE)
-            vol = rotate_cut(slice_vol, PAD_TY1, PAD_UY1, PAD_LX1, PAD_RX1, degree1, center1, IM_SIZE)
-            label_path, image_path = make_paths(IMAGE_PATH,LABEL_PATH, cid, i, idx, "front")
-            sitk.WriteImage(seg, label_path, True)
+            vol,seg=rotate_slice(raw_vol,raw_seg,idx,parser.axis,PADS1,degree1,center1,IM_SIZE)
+            slice
+            # suffix=cancer_flg(seg)
+            suffix=str(get_slice(raw_seg,idx,axis=parser.axis).max())
+            image_path,label_path = make_paths(IMAGE_PATH, cid, i, idx, "front",suffix)
             sitk.WriteImage(vol, image_path, True)
-
+            sitk.WriteImage(seg, label_path, True)
 
         for i, idx in enumerate(reversed(slice2)):
-            slice_seg = raw_seg[:, :, idx]
-            slice_vol = raw_vol[:, :, idx]
-            
-            seg = rotate_cut(slice_seg, PAD_TY2, PAD_UY2, PAD_LX2, PAD_RX2, degree2, center2, IM_SIZE)
-            vol = rotate_cut(slice_vol, PAD_TY2, PAD_UY2, PAD_LX2, PAD_RX2, degree2, center2, IM_SIZE)
-            
-            label_path, image_path = make_paths(IMAGE_PATH,LABEL_PATH, cid, i, idx, "back")
-            sitk.WriteImage(seg, label_path, True)
+            vol,seg=rotate_slice(raw_vol,raw_seg,idx,parser.axis,PADS2,degree2,center2,IM_SIZE)
+            #ガンが含むかどうか
+            suffix=str(get_slice(raw_seg,idx,axis=parser.axis).max())
+
+            image_path,label_path = make_paths(IMAGE_PATH,cid, i, idx, "back",suffix)
             sitk.WriteImage(vol, image_path, True)
+            sitk.WriteImage(seg, label_path, True)
 
         log_df.to_csv('./0701max_slice.csv')
 
 
+
+
+
+def get_parser():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-a', '--axis',type=int,default=0)
+    return parser
+
+
+if __name__ == "__main__":
+    parser=get_parser().parse_args()
+    main(parser)
 
 
---
+++ scripts/Preprocess/resample_spacing.py
@@ -0,0 +1,101 @@
+import glob
+from scipy.ndimage.interpolation import zoom
+import re 
+import SimpleITK as sitk
+import os
+import argparse
+import numpy as np
+from pathlib import Path
+
+"""
+スライスのスペーシングを同じにする
+python3 scripts/Preprocess/resample_spacing.py -i ~/Desktop/kits19/data -o ./data/input/image 
+"""
+
+
+def numericalSort(value):
+    numbers=re.compile(r'(\d+)')
+    parts=numbers.split(value)
+    parts[1::2]=map(int,parts[1::2])
+    return parts
+
+def saveNPY(array,save_path):
+    array=array.astype(np.int16)
+    save_path+=".npy"
+    np.save(save_path,array)
+
+def resample_spacing(image,new_spacing=[1,1,1],interpolator=sitk.sitkLinear):
+    resample=sitk.ResampleImageFilter()
+    #補完方法
+    resample.SetInterpolator(interpolator)
+    resample.SetOutputDirection(image.GetDirection())
+    resample.SetOutputOrigin(image.GetOrigin())
+    resample.SetOutputSpacing(new_spacing)
+
+    new_size=np.array(image.GetSize(),dtype=np.float32)*(np.array(image.GetSpacing(),dtype=np.float32)/new_spacing)
+    new_size=np.ceil(new_size).astype(np.int)
+    new_size=[int(s)for s in new_size]
+    resample.SetSize(new_size)
+
+    return resample.Execute(image)
+
+
+def main(parser):
+
+
+    input_dir=Path(parser.input_dir).resolve()
+    output_dir=Path(parser.output_dir).resolve()
+
+    for i,imagepath in enumerate(sorted(input_dir.glob("*/imaging.nii.gz"))):
+        if i <80:continue
+            
+        print(imagepath)
+        outputfile=output_dir / imagepath.parents[0].stem /'image'
+        os.makedirs(outputfile.parents[0],exist_ok=True)
+        
+        image=sitk.ReadImage(str(imagepath))
+        image_re=image
+        image_re=resample_spacing(image,[1.0,1.0,1.0]) 
+        if parser.save_mha:
+            sitk.WriteImage(image_re,str(outputfile)+'.mha',True)
+        else:
+            image_arr=sitk.GetArrayFromImage(image_re)
+            saveNPY(image_arr,str(outputfile))
+    
+    for labelpath in sorted(input_dir.glob("*/segmentation.nii.gz")):
+        outputfile=output_dir / labelpath.parents[0].stem /'label'
+        os.makedirs(outputfile.parents[0],exist_ok=True)
+        
+        label=sitk.ReadImage(str(labelpath))
+        label_re=label
+        label_re=resample_spacing(label,[1.0,1.0,1.0],interpolator=sitk.sitkNearestNeighbor) 
+        if parser.save_mha:
+            sitk.WriteImage(label_re,str(outputfile)+'.mha',True)
+        else:
+            label_arr=sitk.GetArrayFromImage(label_re)
+            saveNPY(label_arr,str(outputfile))
+
+
+
+
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        prog='generate parallax image using U-Net',
+        usage='python main.py',
+        description='This module　generate parallax image using U-Net.',
+        add_help=True
+    )
+
+    parser.add_argument('-i', '--input_dir', type=str)
+    parser.add_argument('-o', '--output_dir', type=str)
+    parser.add_argument('-m','--save_mha',action='store_true')
+
+                     
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_parser().parse_args()
+    main(parser)
